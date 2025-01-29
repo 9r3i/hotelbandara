e25bb7897c01072fe3c7ece514586fc911606435
9r3i <luthfie@y7mail.com>
2025-01-30 03:09:52 +0700

    Update qr.scanner.module.js

--- js/qr.scanner.module.js
+++ js/qr.scanner.module.js
@@ -1,2 +1,2 @@
-import QrScanner from "https://hotelbandara.com/dev/scanner/qr-scanner.min.js";
+import QrScanner from "https://raw.githubusercontent.com/9r3i/hotelbandara/refs/heads/master/scanner/qr-scanner.min.js";
 window.QrScanner=QrScanner;
